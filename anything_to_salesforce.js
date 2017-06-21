121323e5d48c4e353c5b4662612ce0d03e2b7acf
Taylor Downs <taylor@openfn.org>
2017-06-21 08:54:24 -0400

    Update anything_to_salesforce.js

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -2,7 +2,7 @@
     field("vera__GHI_ID_Number__c", "8675309"),
     field("name", sourceValue("$.data.name")),
     field("vera__Country__c", function(state) {
-        return ''
+        return 'Kenya'
     }),
     // field("vera__Country__c", "always Kenya"),
     relationship("vera__First_Hear_House__r", "vera__house_id__c", "7"),
