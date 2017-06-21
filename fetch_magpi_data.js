570fb8ad0c8018790893b41395419d4335b0b757
Taylor Downs <taylor@openfn.org>
2017-06-21 08:56:15 -0400

    Create fetch_magpi_data.js

---
+++ fetch_magpi_data.js
@@ -0,0 +1,6 @@
+fetchSurveyData({
+  "surveyId": 37479,
+  "afterDate": "2016-01-02",
+  // "beforeDate": "2100-01-01",
+  "postUrl": "https://www.openfn.org/inbox/8ad63a29-5c25-4d8d-ba2c-fe6274dcfbab"
+});
