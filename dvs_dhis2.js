b3d423146161e4b8bed555a6f9138eb83c079daa
Taylor Downs <taylor@openfn.org>
2017-06-28 22:44:26 -0700

    Update dvs_dhis2.js

--- dvs_dhis2.js
+++ dvs_dhis2.js
@@ -1,16 +1,19 @@
-dataValueSet(
-  fields(
-    field("dataSet", "pBOMPrpg1QX"),
-    field("orgUnit", "DiszpKrYNg8"),
-    field("period", "201401"),
-    field("completeData", dataValue("form.date")),
-    field("dataValues", function(state) {
-      console.log("Do anything you want in here.");
-      return [
-        dataElement("qrur9Dvnyt5", state.data.prop_a),
-        dataElement("oZg33kd9taw", state.data.prop_a),
-        dataElement("msodh3rEMJa", state.data.prop_a)
-      ];
-    })
-  )
+each(
+  dataPath("data[*]"),
+  dataValueSet(
+    fields(
+      field("dataSet", "pBOMPrpg1QX"),
+      field("orgUnit", "DiszpKrYNg8"),
+      field("period", "201401"),
+      field("completeData", dataValue("today")),
+      field("dataValues", function(state) {
+        console.log("Do anything you want in here.");
+        return [
+          dataElement("qrur9Dvnyt5", state.data.site_school_number),
+          dataElement("oZg33kd9taw", state.data.light_source),
+          dataElement("msodh3rEMJa", state.data.number_of_children)
+        ];
+      })
+    )
+  );
 );
