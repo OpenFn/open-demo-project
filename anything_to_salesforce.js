ec1cd82df773098291f366a06669827a98222739
Taylor Downs <taylor@openfn.org>
2017-06-29 07:34:33 -0700

    Update anything_to_salesforce.js

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -1,15 +1,17 @@
 each(
   dataPath("data[*]"),
+  // Use system-specific named functions...
   create("vera__Beneficiary__c", fields(
     field("vera__GHI_ID_Number__c", dataValue("site_school_number")),
     field("name", dataValue("parent_surname")),
     field("vera__Country__c", function(state) {
-      console.log(state)
-        if (state.data.village == "Leicester") {
+      // Or do what you want in here...
+      console.log("Hi Mom!)
+      if (state.data.village == "Leicester") {
           return 'England'
-        } else {
-          return "Unknown"
-        }
+      } else {
+        return "Unknown"
+      }
     }),
     relationship("vera__Parents_House__r", "vera__house_id__c", dataValue("parents_house")),
     field("vera__Photo_URL__c", dataValue("photo.url"))
