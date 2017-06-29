b675516dffe7f9f6496a2738a63a529c23f49526
Taylor Downs <taylor@openfn.org>
2017-06-29 07:27:35 -0700

    Update anything_to_salesforce.js

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -1,12 +1,16 @@
-create("vera__Beneficiary__c", fields(
-  field("vera__GHI_ID_Number__c", "site_school_number"),
-  field("name", dataValue("parent_surname")),
-  field("vera__Country__c", function(state) {
-      if (state.data.village == "Leicester") {
-        return 'England'
-      } else {
-        return "Unknown"
-      }
-  }),
-  relationship("vera__Parents_House__r", "vera__house_id__c", dataValue("parents_house"))
-));
+each(
+  dataPath("data[*]"),
+  create("vera__Beneficiary__c", fields(
+    field("vera__GHI_ID_Number__c", dataValue("site_school_number")),
+    field("name", dataValue("parent_surname")),
+    field("vera__Country__c", function(state) {
+      console.log(state)
+        if (state.data.village == "Leicester") {
+          return 'England'
+        } else {
+          return "Unknown"
+        }
+    }),
+    relationship("vera__Parents_House__r", "vera__house_id__c", dataValue("parents_house"))
+  ))
+)
