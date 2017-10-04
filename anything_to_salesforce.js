0ad1f29845e4b333253b54a62af1578e138784a7
Taylor Downs <taylor@openfn.org>
2017-10-04 16:54:04 -0300

    Update anything_to_salesforce.js

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -1,6 +1,6 @@
 each(
   dataPath("data[*]"),
-  // Use tech-specific name functions...
+  // Use tech-specific name functions.... santiago
   create("vera__Beneficiary__c", fields(
     field("vera__GHI_ID_Number__c", dataValue("site_school_number")),
     field("name", dataValue("parent_surname")),
