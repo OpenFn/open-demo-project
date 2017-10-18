4eb10a9a02d894a20d8e9807cbeee148d24b8917
Taylor Downs <taylor@openfn.org>
2017-10-18 14:47:44 -0400

    demo@openfn.org OpenFn Web Edit

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -1,6 +1,6 @@
 each(
   dataPath("data[*]"),
-  // show me the SHA!
+  // show me the SHA! now.
   create("vera__Beneficiary__c", fields(
     field("vera__GHI_ID_Number__c", dataValue("site_school_number")),
     field("name", dataValue("parent_surname")),
