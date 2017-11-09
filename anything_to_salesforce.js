a9c927c050ee6b48801178cd31b372296f77d973
Taylor Downs <taylor@openfn.org>
2017-11-09 10:31:07 -0300

    demo@openfn.org OpenFn Web Edit

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -1,9 +1,9 @@
-each(
-  dataPath("data[*]"),
+// each(
+  // dataPath("data[*]"),
   // Clean.
   create("vera__Beneficiary__c", fields(
     field("vera__GHI_ID_Number__c", dataValue("site_school_number")),
-    field("nadme", dataValue("parent_surname")),
+    field("name", dataValue("parent_surname")),
     field("vera__Gender__c", dataValue("head_of_household_gender")),
     field("vera__Country__c", function(state) {
         // Or do anything in here...ok
@@ -17,4 +17,5 @@ each(
     relationship("vera__Parents_House__r", "vera__house_id__c", dataValue("parents_house")),
     field("vera__photo_url__c", dataValue("photo.url"))
   ))
-)
+  
+// )
