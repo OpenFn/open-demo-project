bdbadc2be178485a15e19546143c68d386d058a6
Taylor Downs <taylor@openfn.org>
2017-11-09 10:29:14 -0300

    demo@openfn.org OpenFn Web Edit

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -3,7 +3,7 @@ each(
   // Clean.
   create("vera__Beneficiary__c", fields(
     field("vera__GHI_ID_Number__c", dataValue("site_school_number")),
-    field("name", dataValue("parent_surname")),
+    field("nadme", dataValue("parent_surname")),
     field("vera__Gender__c", dataValue("head_of_household_gender")),
     field("vera__Country__c", function(state) {
         // Or do anything in here...ok
