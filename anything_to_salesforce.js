408b10381a2907b321a25eef88c8262f52297205
Taylor Downs <taylor@openfn.org>
2017-12-15 09:14:23 -0500

    demo@openfn.org OpenFn Web Edit

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -1,7 +1,7 @@
 each(
   dataPath("data[*]"),
   // Clean. Push to github. again! not now
-  // Push back to OpenFn again.
+  // Push back to OpenFn again.dsadsad
   create("vera__Beneficiary__c", fields(
     field("vera__GHI_ID_Number__c", dataValue("site_school_number")),
     field("name", dataValue("parent_surname")),
