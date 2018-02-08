6a1b611aa2565a8c3d04d6b37c7c38fa9f8050c4
Taylor Downs <taylor@openfn.org>
2018-02-08 19:01:59 -0300

    Update anything_to_salesforce.js

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -5,8 +5,6 @@ each(
     field("name", dataValue("parent_surname")),
     field("vera__Gender__c", dataValue("head_of_household_gender")),
     field("vera__Country__c", function(state) {
-        // Or do anything in here...ok
-        console.log("Hola, Mama!")
         if (state.data.village == "Leicester") {
           return 'England'
         } else {
@@ -16,4 +14,4 @@ each(
     relationship("vera__Parents_House__r", "vera__house_id__c", dataValue("parents_house")),
     field("vera__photo_url__c", dataValue("photo.url"))
   ))
-)
+);
