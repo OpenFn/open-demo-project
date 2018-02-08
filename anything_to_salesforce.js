3988c753c29a8cfa73a58675f8f8ecc7bef65898
Taylor Downs <taylor@openfn.org>
2018-02-08 19:01:24 -0300

    Update anything_to_salesforce.js

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -1,17 +1,12 @@
 each(
   dataPath("data[*]"),
-  // Clean. Push to github. again! not now
-  // Push back to OpenFn again.dsadsad
-  // push to github
-  // santiago, this works!
-//   so does this!!!
   create("vera__Beneficiary__c", fields(
     field("vera__GHI_ID_Number__c", dataValue("site_school_number")),
     field("name", dataValue("parent_surname")),
     field("vera__Gender__c", dataValue("head_of_household_gender")),
     field("vera__Country__c", function(state) {
         // Or do anything in here...ok
-        console.log("Hi, Dad!")
+        console.log("Hola, Mama!")
         if (state.data.village == "Leicester") {
           return 'England'
         } else {
