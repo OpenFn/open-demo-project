e08129f7ae2c6ec202f9a01652323e1efbb9f54b
Taylor Downs <taylor@openfn.org>
2018-03-06 10:18:18 +0000

    taylor@openfn.org OpenFn Web Edit

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -2,9 +2,9 @@ create("foo", fields(
   field("bar", "baz"),
   field("qux", (state) => {
     if (state.data.some_condition) {
-      "everything's gonna be alright"
+      "everything is awesome!"
     } else {
-      throw new Error("Fugees coming to the party tonight");
+      throw new Error("everything is the worst  ¯\_(ツ)_/¯");
     }
   })
 ))
