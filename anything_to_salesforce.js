a72ab0c1437bcbcbe55d8d2a6e927594c5ac4a5e
Taylor Downs <taylor@openfn.org>
2018-04-02 20:14:05 +0000

    demo@openfn.org OpenFn Web Edit

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -1,11 +1,20 @@
-create("foo", fields(
-  field("bar", "baz"),
-  field("qux", (state) => {
-    if (state.data.some_condition) {
-      "everything is awesome!"
-    } else {
-      // throw new Error("everything is the worst  ¯\\\_(ツ)_/¯");
-      console.log("hi")
-    }
-  })
-))
+each(
+  dataPath("data[*]"),
+  // show me the SHA! now.
+  create("vera__Beneficiary__c", fields(
+    field("vera__GHI_ID_Number__c", dataValue("site_school_number")),
+    field("name", dataValue("parent_surname")),
+    field("vera__Gender__c", dataValue("head_of_household_gender")),
+    field("vera__Country__c", function(state) {
+        // Or do anything in here...ok
+        // console.log("Hi, Dad!")
+        if (state.data.village == "Leicester") {
+          return 'England'
+        } else {
+          return "Unknown"
+        }
+    }),
+    relationship("vera__Parents_House__r", "vera__house_id__c", dataValue("parents_house")),
+    field("vera__photo_url__c", dataValue("photo.url"))
+  ))
+)
