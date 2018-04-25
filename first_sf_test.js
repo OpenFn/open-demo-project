172050653013cd3e0721ad31da605efa3654fd73
Taylor Downs <taylor@openfn.org>
2018-04-25 17:42:01 +0000

    taylor@openfn.org OpenFn Web Edit

--- first_sf_test.js
+++ first_sf_test.js
@@ -1,15 +1,15 @@
-throw("oops")
-// console.log("test job")
-// console.log(state)
+// throw("oops")
+console.log("test job")
+console.log(state)
 
-// create("Account", fields(
-//   field("Name", dataValue("from")),
-//   field("Description", dataValue("sampleText")),
-//   field("Website", dataValue("website")),
-//   field("Phone", "867-5309")
-// ))
+create("Account", fields(
+  field("Name", dataValue("from")),
+  field("Description", dataValue("sampleText")),
+  field("Website", dataValue("website")),
+  field("Phone", "867-5309")
+))
 
-// console.log("hi from openfn.org")
-// console.log("hi from github")
-// console.log("hi from openfn again?")
-// console.log("openfn to github")
+console.log("hi from openfn.org")
+console.log("hi from github")
+console.log("hi from openfn again?")
+console.log("openfn to github")
