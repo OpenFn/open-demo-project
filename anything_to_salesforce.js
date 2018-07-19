5a7514058a4a17087eb1785c22078a6afb89747b
Taylor Downs <taylor@openfn.org>
2018-07-19 15:18:03 +0000

    Update anything_to_salesforce.js

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -1,5 +1,5 @@
 query('SELECT Id from vera__House__c'); //from ofn after update, after delete/create
-// from gh after update
+// from gh after update, after delete/create
 
 alterState(state => {
   console.log(state);
