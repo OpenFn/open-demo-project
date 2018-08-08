d3d7662e2a878fb4a02bc956f7d801e75a74f920
Taylor Downs <taylor@openfn.org>
2018-08-08 16:09:49 +0200

    taylor@openfn.org OpenFn Web Edit

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -1,13 +1,3 @@
-// query('SELECT Id from vera__House__c'); //from ofn after update, after delete/create
-// // from gh after update, after delete/create
-
-// // 0penfn demo test?
-
-// alterState(state => {
-//   console.log(state);
-//   return state;
-// })
-
 create("vera__Beneficiary__c", fields(
   // field("vera__GHI_ID_Number__c", state => {
   //   return Date.now();
@@ -16,7 +6,6 @@ create("vera__Beneficiary__c", fields(
   field("vera__Gender__c", dataValue("head_of_household_gender")),
   field("vera__Country__c", function(state) {
       // Or do anything in here...ok
-      // console.log("Hi, Dad!")
       if (state.data.village == "Leicester") {
         return 'England'
       } else {
