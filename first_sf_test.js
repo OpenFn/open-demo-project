39a7dba84c0e662c20c61e027987a418d32a6703
Taylor Downs <taylor@openfn.org>
2018-10-10 09:05:21 +0100

    taylor@openfn.org OpenFn Web Edit

--- first_sf_test.js
+++ first_sf_test.js
@@ -1,9 +1,9 @@
 // testing GH integration from OpenFn, from gh now. taylor testing.
-// create("Account", fields(
-//   field("Name", dataValue("from")),
-//   field("Description", dataValue("sampleText")),
-//   field("Website", dataValue("website")),
-//   field("Phone", "867-5309")
-// ));
+create("Account", fields(
+  field("Name", dataValue("from")),
+  field("Description", dataValue("sampleText")),
+  field("Website", dataValue("website")),
+  field("Phone", "867-5309")
+));
 
-query();
+// query();
