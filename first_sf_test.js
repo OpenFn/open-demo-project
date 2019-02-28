23035186d315763ab004fad84e160c7ca6c79561
Taylor Downs <taylor@openfn.org>
2019-02-28 14:11:23 +0000

    taylor@openfn.org's change on OpenFn.org

--- first_sf_test.js
+++ first_sf_test.js
@@ -1,8 +1,7 @@
-// testindgaad
+// Testing the github commit service.
 create("Account", fields(
   field("Name", dataValue("from")),
   field("Description", dataValue("sampleText")),
   field("Website", dataValue("website")),
   field("Phone", "867-5309")
 ));
-// can i do it as a collab? external one?! 018 taylor
