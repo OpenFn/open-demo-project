be239568a5f7c7c785c64e7e9d665eae73ae3c59
Taylor Downs <taylor@openfn.org>
2019-03-21 13:25:44 +0000

    taylor@openfn.org's change on OpenFn.org

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -1,8 +1,4 @@
 create("vera__Beneficiary__c", {
-  // field("vera__GHI_ID_Number__c", state => {
-  //   return Date.now();
-  // }),
-  // field("name", dataValue("parent_surname")),
   vera__Gender__c: dataValue("super_change"),
   vera__Country__c: function(state) {
       // Or do anything in here...ok
@@ -12,13 +8,5 @@ create("vera__Beneficiary__c", {
         return "Far away"
       }
   },
-  // field("vera__Parents_House__c", state => {
-  //   return state.references[0].records[0].Id;
-  // }),
   vera__photo_url__c: dataValue("photo.url")
-})
-
-alterState(state => {
-  console.log(state)
-  return state;
 });
