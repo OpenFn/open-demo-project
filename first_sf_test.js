b6e58436983f3f972f0b5be220c523f92837c543
Taylor Downs <taylor@openfn.org>
2019-06-12 13:34:05 +0100

    taylor@openfn.org's change on OpenFn.org

--- first_sf_test.js
+++ first_sf_test.js
@@ -1,5 +1,5 @@
 // Testing the github commit service!!
-csreate("Account", fields(
+create("Account", fields(
   field("Name", dataValue("from")),
   field("Description", dataValue("sampleText")),
   field("Website", dataValue("website")),
