67512e0d412085f450565954e94fbbd678a79c4d
Taylor Downs <taylor@openfn.org>
2019-07-15 18:55:39 +0200

    Update anything_to_salesforce.js

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -1,7 +1,7 @@
 create("vera__Beneficiary__c", {
   vera__Gender__c: dataValue("super_change"),
   vera__Country__c: function(state) {
-      // Or do anything in here... ok, from ch
+      // Or do anything in here... ok.
       if (state.data.village == "Leicester") {
         return 'England'
       } else {
