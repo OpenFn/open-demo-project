90296440a6ac30d6191660cf3750059a2f98a4c4
Taylor Downs <taylor@openfn.org>
2019-09-20 07:44:35 +0100

    taylor@openfn.org's change on OpenFn.org

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -5,6 +5,7 @@ create("vera__Beneficiary__c", {
       if (state.data.village == "Leicester") {
         return 'England'
       } else {
+        setTimeout(function(){ console.log("did i?"); }, 3000);
         console.log(Math.random());
         return "Far away"
       }
