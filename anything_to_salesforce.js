b79e1d0c432f87bfbf97c7811397431187875ac3
juliabuzan <julia.buzan@gmail.com>
2020-06-15 22:23:24 -0400

    demo@openfn.org's change on OpenFn.org

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -1,20 +1,20 @@
 console.log(__node_version);
 // after the github issue was fixed?
 console.log(this.version);
-// create("vera__Beneficiary__c", {
-//   vera__Gender__c: dataValue("gender"),
-//   vera__Country__c: function(state) {
-//       // Or do anything in here...
-//       if (state.data.village == "Leicester") {
-//         return 'England'
-//       } else {
-//         setTimeout(() => {
-//           console.log("did i?");
-//         }, 3000);
-//         console.log(Math.random());
-//         return "Far away"
-//       }
-//   },
-//   vera__photo_url__c: dataValue("photo.url")
-// })
+create("vera__Beneficiary__c", {
+  vera__Gender__c: dataValue("gender"),
+  vera__Country__c: function(state) {
+      // Or do anything in here...
+      if (state.data.village == "Leicester") {
+        return 'England'
+      } else {
+        setTimeout(() => {
+          console.log("did i?");
+        }, 3000);
+        console.log(Math.random());
+        return "Far away"
+      }
+  },
+  vera__photo_url__c: dataValue("photo.url")
+})
 console.log("Changes from GitHub succeeds with existing integrations.");
