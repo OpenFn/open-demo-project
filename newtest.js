fb6fa2d9b1d9b027947474127bbd7b4e2e916bb2
Taylor Downs <taylor@openfn.org>
2020-10-28 17:13:06 +0000

    taylor@localhost's change on OpenFn.org

--- newtest.js
+++ newtest.js
@@ -1,1 +1,18 @@
-// looks fun?
+alterState(state => {
+  console.log(state);
+  setTimeout(() => { console.log("_something in here?"); }, 50);
+  console.log("any other change");
+  return { ...state, thing: 7 };
+  // const xx = "after return";
+  // const x = "other thing!";
+  const x = 'thing here?';
+});
+
+create("Account", fields(
+  field("Name", dataValue("from")),
+  // relationship("Business_Group__r", "BG_UUID__c", dataValue("DestAcct")),
+  // ok there.
+  field("Description", dataValue("sampleText")),
+  field("Website", dataValue("website")),
+  field("Phone", "867-5309")
+));
