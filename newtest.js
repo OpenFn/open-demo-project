b79a02dee9ab02cf270da5262df5750c5e732a1a
Taylor Downs <taylor@openfn.org>
2021-02-17 06:51:13 +0000

    taylor@localhost's change on OpenFn.org

--- newtest.js
+++ newtest.js
@@ -1,13 +1,5 @@
-alterState(state => {
-  console.log(state)
-  return state;
-  // setTimeout(function(){
-  //   return state;
-  // }, 4);
-});
 create("Account", fields(
   field("Name", dataValue("from")),
-  // relationship("Business_Group__r", "BG_UUID__c", dataValue("DestAcct")),
   field("Description", dataValue("sampleText")),
   field("Website", dataValue("website")),
   field("Phone", "867-5309")
