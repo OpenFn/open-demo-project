968f985d11864124aaa92ad59dfbc329f80418ba
Taylor Downs <taylor@openfn.org>
2021-08-31 13:42:32 -0600

    taylor@openfn.org's change on OpenFn.org

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -30,21 +30,21 @@ fn(state => {
   });
 });
 
-// create("vera__Beneficiary__c", fields(
-//   field('vera__Gender__c', dataValue("gender")),
-//   field('vera__Country__c', state => {
-//       // Or do anything in here...
-//       if (state.data.village == "Leicester") {
-//         return 'England';
-//       } else {
-//         setTimeout(() => {
-//           console.log("did i?");
-//         }, 50);
-//         console.log(Math.random());
-//         return "Far away";
-//       }
-//   }),
-//   field('vera__photo_url__c', dataValue("photo.url"))
-// ));
+create("vera__Beneficiary__c", fields(
+  field('vera__Gender__c', dataValue("gender")),
+  field('vera__Country__c', state => {
+      // Or do anything in here...
+      if (state.data.village == "Leicester") {
+        return 'England';
+      } else {
+        setTimeout(() => {
+          console.log("did i?");
+        }, 50);
+        console.log(Math.random());
+        return "Far away";
+      }
+  }),
+  field('vera__photo_url__c', dataValue("photo.url"))
+));
 
 // console.log("Changes from GitHub succeeds with existing integrations 2 also.");
