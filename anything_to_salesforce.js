3d7d9591455ca8cb7bcc5a1fa11b74ad5d52b09b
Taylor Downs <taylor@openfn.org>
2021-10-15 08:41:07 +0100

    demo@openfn.org's change on OpenFn.org

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -3,14 +3,14 @@
 // console.log(this.version);
 // console.log(state);
 
-// fn(state => {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log("wait, and then return");
-//       resolve(state);
-//     }, 2000);   
-//   });
-// });
+fn(state => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("wait, and then return");
+      resolve(state);
+    }, 2000);   
+  });
+});
 
 // fn(state => {
 //   return new Promise((resolve, reject) => {
@@ -41,7 +41,7 @@ create("vera__Beneficiary__c", fields(
           console.log("did i?");
         }, 50);
         console.log(Math.random());
-        return "ok"
+        return "ok";
         // return "star� � 2021-07-29";
       }
   }),
