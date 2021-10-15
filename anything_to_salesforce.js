8b7ea842791b7cd22a35d38b129f11b12032241f
Taylor Downs <taylor@openfn.org>
2021-10-15 08:40:24 +0100

    demo@openfn.org's change on OpenFn.org

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -3,32 +3,32 @@
 // console.log(this.version);
 // console.log(state);
 
-fn(state => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("wait, and then return");
-      resolve(state);
-    }, 2000);   
-  });
-});
+// fn(state => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("wait, and then return");
+//       resolve(state);
+//     }, 2000);   
+//   });
+// });
 
-fn(state => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("wait, and then return 2");
-      resolve(state);
-    }, 2000);   
-  });
-});
+// fn(state => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("wait, and then return 2");
+//       resolve(state);
+//     }, 2000);   
+//   });
+// });
 
-fn(state => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("wait, and then return 3");
-      resolve(state);
-    }, 2000);   
-  });
-});
+// fn(state => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("wait, and then return 3");
+//       resolve(state);
+//     }, 2000);   
+//   });
+// });
 
 // create("vera__Beneficiary__c", fields(
 //   field('vera__Gender__c', dataValue("gender")),
