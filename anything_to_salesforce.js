f70242d2136aa8a40b74b796b23c4f7a03436b2b
Taylor Downs <taylor@openfn.org>
2021-10-15 12:48:10 +0100

    demo@openfn.org's change on OpenFn.org

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -3,32 +3,32 @@
 // console.log(this.version);
 // console.log(state);
 
-fn(state => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("wait, and then return");
-      resolve(state);
-    }, 2000);   
-  });
-});
+// fn(state => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("wait, and then return");
+//       resolve(state);
+//     }, 2000);   
+//   });
+// });
 
-fn(state => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("wait, and then return 2");
-      resolve(state);
-    }, 2000);   
-  });
-});
+// fn(state => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("wait, and then return 2");
+//       resolve(state);
+//     }, 2000);   
+//   });
+// });
 
-fn(state => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("wait, and then return 3");
-      resolve(state);
-    }, 2000);   
-  });
-});
+// fn(state => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("wait, and then return 3");
+//       resolve(state);
+//     }, 2000);   
+//   });
+// });
 
 create("vera__Beneficiary__c", fields(
   field('vera__Gender__c', dataValue("gender")),
@@ -41,11 +41,10 @@ create("vera__Beneficiary__c", fields(
           console.log("did i?");
         }, 50);
         console.log(Math.random());
-        return "ok";
-        // return "star� � 2021-07-29";
+        return "Far away";
       }
   }),
   field('vera__photo_url__c', dataValue("photo.url"))
 ));
 
-// // console.log("Changes from GitHub succeeds with existing integrations 2 also.");
+// console.log("Changes from GitHub succeeds with existing integrations 2 also.");
