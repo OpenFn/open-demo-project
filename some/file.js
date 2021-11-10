0c2c9ff77b90d0275bbdbc4d0ddda56e1501ea86
Taylor Downs <taylor@openfn.org>
2021-11-10 18:01:08 +0100

    demo@openfn.org's change on OpenFn.org

--- some/file.js
+++ some/file.js
@@ -1,1 +1,15 @@
-// nothing special. just a new file.
+alterState(state => { //change no gh filepaths
+  console.log("first op worked great!")
+  let arr = Array(1e8).fill("some string");
+  arr.reverse();
+  return state;
+});
+
+alterState(state => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("wait, and then return");
+      resolve(state);
+    }, 400);   
+  });
+});
