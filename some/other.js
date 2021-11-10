12a3239b5f22392f8350c546a4523f89c1579e8c
Taylor Downs <taylor@openfn.org>
2021-11-10 18:12:42 +0100

    demo@openfn.org's change on OpenFn.org

--- some/other.js
+++ some/other.js
@@ -1,1 +1,47 @@
-another!
+createTEI({
+  "trackedEntityType": "Zy2SEgA61ys",
+  "orgUnit": "DiszpKrYNg8",
+  "attributes": [
+    {
+      "attribute": "w75KJ2mc4zz",
+      "value": function(state) {
+        return 'aleksa';
+      }
+    },
+    {
+      "attribute": "zDhUuAYrxNC",
+      "value": "Smith"
+    }
+  ]
+}
+  )
+
+/*createTEI({
+    "trackedEntityType": "nEenWmSyUEp",
+    "orgUnit": "g8upMTyEZGZ",
+    "attributes": [
+      /*{
+        "attribute": "w75KJ2mc4zz",
+        "value": dataValue("data.respondent_first_name")(state)
+      },
+      {
+        "attribute": "zDhUuAYrxNC",
+        "value": dataValue("data.respondent_surname")(state)
+      }*/
+   // ]/*,
+   /* "enrollments": [
+      {
+          "orgUnit": "TSyzvBiovKh",
+          "program": "IpHINAT79UW",
+          "enrollmentDate": dataValue("data.survey_date")(state)
+       }
+     ],
+     "events":[
+       {
+        "program": "IpHINAT79UW",
+        "orgUnit": "YuQRtpLP10I",
+        "eventDate": dataValue("data.survey_date")(state),
+        "status": "COMPLETED"
+       }
+       //]
+  })*/
