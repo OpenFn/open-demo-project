ccdfdc80afa31cff5e399e685b9f2fd0a656b184
Taylor Downs <taylor@openfn.org>
2021-11-29 16:13:26 +0000

    demo@openfn.org's change on OpenFn.org

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -1,6 +1,6 @@
 // console.log(this.version);
 // console.log(state);
-// new scheme for oauth works in both directions
+// new scheme for oauth works in both directions after resync
 
 fn(state => {
   return new Promise((resolve, reject) => {
