ef325c51782a803bfe85cababe69468be2e9ad10
Taylor Downs <taylor@openfn.org>
2021-12-13 16:04:50 +0000

    taylor@openfn.org's change on OpenFn.org

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -1,16 +1,5 @@
 create("vera__Beneficiary__c", fields(
   field('vera__Gender__c', dataValue("gender")),
-  field('vera__Country__c', state => {
-      // Or do anything in here...
-      if (state.data.village == "Leicester") {
-        return 'England';
-      } else {
-        setTimeout(() => {
-          console.log("did i?");
-        }, 50);
-        console.log(Math.random());
-        return "Far away";
-      }
-  }),
+  field('vera__Country__c', "something"),
   field('vera__photo_url__c', dataValue("photo.url"))
 ));
