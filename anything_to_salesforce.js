b92de56de67d29e704a395d7e5335a25d8bc8464
Taylor Downs <taylor@openfn.org>
2021-12-13 16:26:31 +0000

    taylor@openfn.org's change on OpenFn.org

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -1,5 +1,5 @@
 create("vera__Beneficiary__c", fields(
-  field('vera__Gender__c', dataValue("gender")),
-  field('vera__Country__c', "something"),
-  field('vera__photo_url__c', dataValue("photo.url"))
+  field('vera__Gender__c', dataValue('gender')),
+  field('vera__Country__c', "hard code some value!"),
+  field('vera__photo_url__c', dataValue('photo.url'))
 ));
