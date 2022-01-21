8f9777fde2e7c751c386eee27f6f0a82108335a1
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2022-01-21 17:05:03 -0600

    aissatou@openfn.org's change on OpenFn.org

--- code/upsert_referral_in_primero.js
+++ code/upsert_referral_in_primero.js
@@ -1,1 +1,13 @@
-
+// Your job goes here.
+updateReferrals(
+  {
+    externalId: 'case_id',
+    id: '2a2cc932-4b6f-43a6-928d-40fc9949eb02',
+    referral_id: 'c377bcb2-122d-46ee-a861-869e1b6476cd',
+    data: state => state.data 
+  },
+  state => {
+    console.log(state.data); //log data
+    return state;
+  }
+);
