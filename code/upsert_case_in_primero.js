c7f0db9dc59ed174c51e18b3b213605019807a4c
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2022-01-21 17:02:27 -0600

    aissatou@openfn.org's change on OpenFn.org

--- code/upsert_case_in_primero.js
+++ code/upsert_case_in_primero.js
@@ -1,7 +1,7 @@
 // Your job goes here.
  upsertCase({
     externalId: "234bbac3-7278-430a-98b5-1abc460671ec", //upsert 1 case using case id
-     data: state => state.data 
+    data: state => state.data 
 }, 
   state => {
     console.log(state.data);
