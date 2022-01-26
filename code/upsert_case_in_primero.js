44401f4bfbd270440c1266e97960b9188c705bc1
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2022-01-26 07:09:38 -0600

    aissatou@openfn.org's change on OpenFn.org

--- code/upsert_case_in_primero.js
+++ code/upsert_case_in_primero.js
@@ -3,8 +3,8 @@
     externalId: 'case_id', //upsert 1 case using case id
     data: {
        "age": 20,
-       "sex": "male",
-       "name": "Alex",
+       "sex": "female",
+       "name": "Aicha",
        "status": "open",
        "case_id": "234bbac3-7278-430a-98b5-1abc460671ec",
  }}, 
