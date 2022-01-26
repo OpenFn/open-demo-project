271c240b51d12014c89a2b18fac216870441d320
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2022-01-26 07:10:41 -0600

    aissatou@openfn.org's change on OpenFn.org

--- code/upsert_referral_in_primero.js
+++ code/upsert_referral_in_primero.js
@@ -4,8 +4,13 @@ updateReferrals(
     externalId: 'case_id',
     id: '2a2cc932-4b6f-43a6-928d-40fc9949eb02',
     referral_id: 'c377bcb2-122d-46ee-a861-869e1b6476cd',
-    data: state => state.data 
-  },
+    data: {
+       "age": 20,
+       "sex": "female",
+       "name": "Aicha",
+       "status": "open",
+       "case_id": "234bbac3-7278-430a-98b5-1abc460671ec",
+ } },
   state => {
     console.log(state.data); //log data
     return state;
