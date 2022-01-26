05ad7608cb3a0fd7e6438a7c2820dae87b763c78
daissatou2 <80456839+daissatou2@users.noreply.github.com>
2022-01-26 04:28:09 -0600

    aissatou@openfn.org's change on OpenFn.org

--- code/upsert_case_in_primero.js
+++ code/upsert_case_in_primero.js
@@ -1,9 +1,15 @@
 // Your job goes here.
  upsertCase({
     externalId: 'case_id', //upsert 1 case using case id
-    data: state => state.data 
-}, 
+    data: {
+       "age": 20,
+       "sex": "male",
+       "name": "Alex",
+       "status": "open",
+       "case_id": "234bbac3-7278-430a-98b5-1abc460671ec",
+ }}, 
   state => {
     console.log(state.data); //log data
     return state;
   });
+  
