3bcb4a5740ff82c4ceaeb74e44429757cfcd798b
Taylor Downs <taylor@openfn.org>
2022-02-01 18:37:15 +0000

    demo@openfn.org's change on OpenFn.org

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -1,12 +1,20 @@
-console.log(this.version);
-console.log(dataValue('[2  ]')(state));
+create("vera__Beneficiary__c", fields(
+  field('vera__Gender__c', dataValue("gender")),
+  field('vera__Country__c', state => {
+      // Or do anything in here...
+      if (state.data.village == "Leicester") {
+        return 'England';
+      } else {
+        setTimeout(() => {
+          console.log("did i?");
+        }, 50);
+        console.log(Math.random());
+        return "Far away";
+      }
+  }),
+  field('vera__photo_url__c', dataValue("photo.url"))
+));
 
-console.log(
-  dataValue('[2  ]')(state),
-dataValue('[1]')(state),
-  dataValue("[' baz']")(state),
-dataValue('far.registrations[0][1].age')(state),
-dataValue("['-ad']")(state))
 // new scheme for oauth works in both directions with PRC and frontEnd 2
 // fn(state => {
 //   return new Promise((resolve, reject) => {
@@ -25,22 +33,3 @@ dataValue("['-ad']")(state))
 //     }, 2000);   
 //   });
 // });
-
-// create("vera__Beneficiary__c", fields(
-//   field('vera__Gender__c', dataValue("gender")),
-//   field('vera__Country__c', state => {
-//       // Or do anything in here...
-//       if (state.data.village == "Leicester") {
-//         return 'England';
-//       } else {
-//         setTimeout(() => {
-//           console.log("did i?");
-//         }, 50);
-//         console.log(Math.random());
-//         return "Far away";
-//       }
-//   }),
-//   field('vera__photo_url__c', dataValue("photo.url"))
-// ));
-
-// console.log("Changes from GitHub succeeds with existing integrations 2 also.");
