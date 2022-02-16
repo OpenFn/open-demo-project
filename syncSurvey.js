a8e3a3dc82f7ffecd9d0786bdbfcf639632c3e0e
Aleksa Krolls <aleksagk@gmail.com>
2022-02-16 18:39:08 +0200

    vmasanas@disgrafic.com's change on OpenFn.org

--- syncSurvey.js
+++ syncSurvey.js
@@ -1,1 +1,7 @@
 // Your job goes here.
+upsert('demo_survey', 'AnswerId',{
+  'AnswerId': dataValue('body._id'),
+  'SurveyName': dataValue('body.survey'),
+  'KoboForm':dataValue('body.formName')
+});
+
