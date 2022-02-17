35f61100e7f32d54564c437cf662988b5e21400d
lakhassane <mamadou@openfn.org>
2022-02-17 16:37:50 +0000

    mamadou@openfn.org's change on OpenFn.org

--- registerPersonDB.js
+++ registerPersonDB.js
@@ -1,1 +1,18 @@
 // Map person and service data to the DB
+fn(state => {
+  const campMapping = {
+    1122: 'Yida',
+    1123: 'Zaatari'
+  }
+  
+  const mapping = {
+    External_ID: dataValue('case_id')(state),
+    consent: dataValue('consent')(state),
+    age: dataValue('age')(state),
+    camp: campMapping[dataValue('camp')(state)]
+  }
+  
+  console.log(mapping);
+  
+  return state;
+})
