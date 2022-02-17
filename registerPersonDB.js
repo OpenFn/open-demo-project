2da27a1cbbbd1550de0869b9bd3d5cc4d8297b41
lakhassane <mamadou@openfn.org>
2022-02-17 16:56:45 +0000

    mamadou@openfn.org's change on OpenFn.org

--- registerPersonDB.js
+++ registerPersonDB.js
@@ -10,12 +10,25 @@ fn(state => {
     consent: dataValue('consent')(state) === "yes" ? 1 : 0,
     age: undefined,
     camp: campMapping[dataValue('camp')(state)]
+    // Person_ID : unknown
+    // select from demo_person where External_ID = case_id
   }
   
   return {...state, mapping};
 })
 
-upsert('demo_person', 'External_ID', state => state.mapping, { setNull: "'undefined'", logValues: true })
+upsert('demo_person', 'External_ID', state => state.mapping, { setNull: "'undefined'", logValues: true }
+)
 
-
+upsertMany('demo_service', 'External_ID', state => state.services_section.map(service => {
+  return {
+    Type: service.service_type,
+    External_ID: service.unique_id,
+    Person_ID: findValue({
+       uuid: 'Person_ID',
+       relation: 'demo_person',
+       where: { External_ID: dataValue('case_id')(state) },
+     }),
+  }
+}) )
 
