c097da561bc0ca88ba6685f41dc14fa7037e57d6
Aleksa Krolls <aleksagk@gmail.com>
2022-02-24 17:41:05 +0200

    vmasanas@disgrafic.com's change on OpenFn.org

--- test-sample-feb24.js
+++ test-sample-feb24.js
@@ -1,2 +1,30 @@
 // Your job goes here.
 // test
+fn(state => {
+  const person = 
+      {
+        'External_ID': dataValue('case_id')(state),
+        'Consent': (dataValue('consent')(state) === "yes" ? 1 : 0),
+        'Age': dataValue('age')(state),
+        'Camp': dataValue('camp')(state)
+      };
+    
+  
+  
+  
+  const records = [
+      {
+        'AnswerId': dataValue('body._id'),
+        'SurveyName': dataValue('body.survey'),
+        'KoboForm':dataValue('formName')
+      },
+      {
+        'AnswerId': dataValue('body._id') + '2nd',
+        'SurveyName': dataValue('body.survey') + '2nd',
+        'KoboForm':dataValue('formName') + '2nd'
+      },
+    
+    ]
+    
+    return {...state, records }
+})
