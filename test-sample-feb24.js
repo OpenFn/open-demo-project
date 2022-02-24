ad806e88ae05e2f8ed928f591498c9170d949772
Aleksa Krolls <aleksagk@gmail.com>
2022-02-24 17:47:54 +0200

    vmasanas@disgrafic.com's change on OpenFn.org

--- test-sample-feb24.js
+++ test-sample-feb24.js
@@ -1,30 +1,22 @@
 // Your job goes here.
 // test
 fn(state => {
+  const camps = {
+    1122 : "Yida",
+    1123 : "Zaatari"
+  }
+  
   const person = 
       {
         'External_ID': dataValue('case_id')(state),
         'Consent': (dataValue('consent')(state) === "yes" ? 1 : 0),
         'Age': dataValue('age')(state),
-        'Camp': dataValue('camp')(state)
+        'Camp': camps[dataValue('camp')(state)]
       };
     
-  
-  
-  
-  const records = [
-      {
-        'AnswerId': dataValue('body._id'),
-        'SurveyName': dataValue('body.survey'),
-        'KoboForm':dataValue('formName')
-      },
-      {
-        'AnswerId': dataValue('body._id') + '2nd',
-        'SurveyName': dataValue('body.survey') + '2nd',
-        'KoboForm':dataValue('formName') + '2nd'
-      },
-    
-    ]
-    
-    return {...state, records }
+    return {...state, person }
 })
+
+upsert('demo_person', 'External_ID', (state) => state.person);
+
+
