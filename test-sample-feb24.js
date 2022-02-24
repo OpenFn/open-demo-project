5ec70665d55b0c168e57f062b9fa08fb546cc607
Aleksa Krolls <aleksagk@gmail.com>
2022-02-24 18:02:32 +0200

    vmasanas@disgrafic.com's change on OpenFn.org

--- test-sample-feb24.js
+++ test-sample-feb24.js
@@ -1,22 +1,52 @@
-// Your job goes here.
-// test
+
+// first do the mappings on an fn() block
+// the the upserts
+
 fn(state => {
   const camps = {
     1122 : "Yida",
     1123 : "Zaatari"
   }
   
-  const person = 
-      {
+  const person =  {
         'External_ID': dataValue('case_id')(state),
         'Consent': (dataValue('consent')(state) === "yes" ? 1 : 0),
         'Age': dataValue('age')(state),
         'Camp': camps[dataValue('camp')(state)]
-      };
-    
-    return {...state, person }
+  };
+  
+  return {...state, person }
 })
 
 upsert('demo_person', 'External_ID', (state) => state.person);
+
+
+
+
+fn(state => {
+  var services = []
+  
+  for(var service of state.data.services_section)
+  {
+    services.push(
+      {
+        'Type': service.service_type,
+        'External_ID': service.unique_id,
+        'Person_ID': findValue({
+           uuid: 'Person_ID',
+           relation: 'demo_person',
+           where: { External_ID: state.data.case_id}
+         })(state)
+      }
+    )
+  }
+  
+  return {...state, services }
+})
+
+
+upsertMany(
+ 'demo_service', 'External_ID', (state)=> state.services
+);
 
 
