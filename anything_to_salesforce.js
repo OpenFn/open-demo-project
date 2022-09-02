f4afbf568ea5a7e6685d6b66c055c557afac34e9
Taylor Downs <taylor@openfn.org>
2022-09-02 06:56:30 +0100

    taylor@openfn.org's change on OpenFn.org

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -1,28 +1,40 @@
-fn(state => {
-  if (false) return state;
-  
-  return execute(
-  // new scheme for oauth works in both directions with PRC and frontEnd 2
+
+// new scheme for oauth works in both directions with PRC and frontEnd 2
 fn(state => {
   // throw "bang"
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log(state);
-      console.log(Date.now(), 'CONN_RECVD |> PIPE FROM T-GRN001...')
+      console.log("wait, and then return please");
       resolve(state);
     }, 2000);   
   });
-}),
+});
 
-fn(state => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log(Date.now(), 'TRAIN-GRN001: DEPARTED \ 321#bdsahjk31h->>stop')
-      resolve(state);
-    }, 2000);   
-  });
-})
-)(state)
-  
-  
-})
+// fn(state => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("wait, and then return 3");
+//       resolve(state);
+//     }, 2000);   
+//   });
+// });
+
+// create("vera__Beneficiary__c", fields(
+//   field('vera__Gender__c', dataValue("gender")),
+//   field('vera__Country__c', state => {
+//       // Or do anything in here...
+//       if (state.data.village == "Leicester") {
+//         return 'England';
+//       } else {
+//         setTimeout(() => {
+//           console.log("did i?");
+//         }, 50);
+//         console.log(Math.random());
+//         return "Far away";
+//       }
+//   }),
+//   field('vera__photo_url__c', dataValue("photo.url"))
+// ));
+
+// console.log("Changes from GitHub succeeds with existing integrations 2 also.");
+// local
