6b52caa82a0effc15f8943716a6461a94089c0b2
Taylor Downs <taylor@openfn.org>
2022-10-25 07:42:41 +0100

    taylor@openfn.org's change on OpenFn.org

--- anything_to_salesforce.js
+++ anything_to_salesforce.js
@@ -1,24 +1,3 @@
-
-// new scheme for oauth works in both directions with PRC and frontEnd 2
-// fn(state => {
-//   // throw "bang"
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log("wait, and then return please");
-//       resolve(state);
-//     }, 2000);   
-//   });
-// });
-
-// fn(state => {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log("wait, and then return 3");
-//       resolve(state);
-//     }, 2000000);   
-//   });
-// }); //ok
-
 create("vera__Beneficiary__c", fields(
   field('vera__Gender__c', dataValue("gender")),
   field('vera__Country__c', state => {
@@ -35,6 +14,3 @@ create("vera__Beneficiary__c", fields(
   }),
   field('vera__photo_url__c', dataValue("photo.url"))
 ));
-
-// console.log("Changes from GitHub succeeds with existing integrations 2 also.");
-// local test 2-sep-2022 again
